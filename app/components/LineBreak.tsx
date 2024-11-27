477404da761a14a7aa6616a06f039957564956c4
AbdulselamAbdurehman <abdulselamabdurehman@gmail.com>
2024-11-27 10:23:07 +0300

    Add LineBreakComponent

---
+++ app/components/LineBreak.tsx
@@ -0,0 +1,7 @@
+export default function LineBreak() {
+  return (
+    <div className="flex justify-center">
+      <div className="h-0.5 w-2/5 bg-slate-300 text-center rounded"></div>
+    </div>
+  );
+}
